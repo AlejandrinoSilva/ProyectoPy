bc72a1a7b45082906991c0b19e539628d8f01b17
programmer <programmer@dhcppc14>
2019-10-06 15:55:38 -0300

    Se agregaron cajas de textos sin quitar menu

--- firstScreenMenu.py
+++ firstScreenMenu.py
@@ -19,26 +19,53 @@ class ventanaPrincipal(wx.Frame):
         menubar = wx.MenuBar()
         # Creamos el Menu
         archivo = wx.Menu()
-        archivo.Append(wx.ID_FILE, '&File')
-        archivo.Append(wx.ID_EDIT, '&Edit')
-        archivo.Append(wx.ID_SAVE, '&Save')
-        archivo.Append(wx.ID_HELP, '&Help')
+        archivo.Append(wx.ID_FILE, '&Archivo')
+        archivo.Append(wx.ID_EDIT, '&Editar')
+        archivo.Append(wx.ID_SAVE, '&Guardar')
+        archivo.Append(wx.ID_HELP, '&Ayuda')
         archivo.AppendSeparator()
 
         edit = wx.Menu()
         edit.Append(wx.ID_ANY, "&XItem")
         edit.Append(wx.ID_ANY, "&YItem")
         edit.Append(wx.ID_ANY, "&ZItem")
-
-        archivo.AppendMenu(wx.ID_ANY, 'E&ditar', edit)
+        # Original archivo.AppendMenu(wx.ID_ANY, '&editar', edit)
+        archivo.Append(wx.ID_ANY, '&editar', edit)
 
         opcion = wx.MenuItem(archivo, wx.ID_ANY, '&Quit')
-        archivo.AppendItem(opcion)
+        # Original archivo.AppendItem(opcion)
+        archivo.Append(opcion)
 
         self.Bind(wx.EVT_MENU, self.OnQuit, opcion)
         menubar.Append(archivo, '&Archivo')
         self.SetMenuBar(menubar)
+
+
+        # ========================
+        self.panel = wx.Panel(self)
+        self.sizer = wx.GridBagSizer(3,2)
+
+        self.texto = wx.StaticText(self.panel, label="Nombre:")
+        self.sizer.Add(self.texto, pos=(0, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=5)
+
+        self.textonuevo = wx.StaticText(self.panel, label="Me llamo: ")
+        self.sizer.Add(self.textonuevo, pos=(1, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=5)
+
+        self.textoedit = wx.TextCtrl(self.panel)
+        self.sizer.Add(self.textoedit, pos=(0, 1), span=(1, 3), flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=5)
+
+        self.boton = wx.Button(self.panel, label="Enviar", size=(90, 50))
+        self.sizer.Add(self.boton, pos=(3,3), flag=wx.RIGHT | wx.BOTTOM)
+
+        self.panel.Bind(wx.EVT_BUTTON, self.TomarTexto, self.boton)
+        self.panel.SetSizerAndFit(self.sizer)
+
         self.Show(True)
+
+    def TomarTexto(self, event):
+        textomado = 'Hola Mundo'
+        textomado = self.textoedit.GetValue()
+        self.textonuevo.SetLabel(textomado)
 
     def OnQuit(self, e):
         self.Close()
